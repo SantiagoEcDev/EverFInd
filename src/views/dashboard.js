154524d57abd46300435e2ab0eba91d7c4c13e61
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-10-07 12:08:25 -0500

    Notifications Box

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -87,3 +87,19 @@ aplicarBtn.addEventListener('click', () => {
         aplicarBtn.innerHTML = 'Aplicar  <i class="fa-solid fa-plus"></i>';
     }
 });
+
+var box  = document.getElementById('box');
+var down = false;
+
+
+function toggleNotifi(){
+	if (down) {
+		box.style.height  = '0px';
+		box.style.opacity = 0;
+		down = false;
+	}else {
+		box.style.height  = '510px';
+		box.style.opacity = 1;
+		down = true;
+	}
+}
