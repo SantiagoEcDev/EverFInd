fde318daf30b42ac858eda94ecae89c70c1f1e4c
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-11-09 14:34:23 -0500

    changes uri

--- src/database.js
+++ src/database.js
@@ -5,5 +5,5 @@ const { mongodb } = require('./keys');
 mongoose.connect(mongodb.URI, {
       
 })
-    .then(db => console.log('Database is connected'))
+    .then(db => console.log('Connected to MongoDb Atlas'))
     .catch(err => console.error(err))
--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongodb: {
-        URI: 'mongodb+srv://Admin:admin@cluster0.xntf4w4.mongodb.net/?retryWrites=true&w=majority'
+        URI: 'mongodb://mongo:GF1G2Bg42gedhed5134666ceAC45DAeb@monorail.proxy.rlwy.net:14420'
     }
 }
