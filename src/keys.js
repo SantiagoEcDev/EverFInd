cf0c84891a495140638386415b274d7ffb57906a
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-11-08 22:06:42 -0500

    add mongo

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongodb: {
-        URI: 'mongodb://localhost:27017/everfind'
+        URI: 'mongodb+srv://Admin:Santiago1904*@cluster0.xntf4w4.mongodb.net/?retryWrites=true&w=majority'
     }
 }
