0db3fbd783c5283d2d40cc73b14db49d141355a4
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-11-11 13:57:17 -0500

    Built admin front

--- src/adminModal.js
+++ src/adminModal.js
@@ -14,6 +14,8 @@ var mailCount = document.getElementById("mailCount");
 var notificationIcon = document.getElementById("notificationIcon");
 var notificationCount = document.getElementById("notificationCount");
 
+var sendMessage = document.getElementById("sendMessage")
+
 // Cuando el usuario hace clic en el botón, abre el modal 
 
 mail.addEventListener("click", function() {
@@ -41,8 +43,6 @@ span.onclick = function () {
     userModal.style.display = "none";
 }
 
-
-
 // Cuando el usuario hace clic en cualquier lugar fuera del modal, ciérralo
 
 openMessageModal.onclick = function () {
@@ -61,7 +61,34 @@ window.onclick = function (event) {
     }
 }
 
+// Obtén el modal
+var modalEnvio = document.getElementById("confirmacionEnvioModal");
 
+// Obtén el formulario
+var form = document.getElementById("messageForm");
+
+// Función para mostrar el modal y ocultarlo después de un tiempo
+function mostrarYCerrarModal() {
+    modalEnvio.style.display = "block";
+    setTimeout(function() {
+        modalEnvio.style.display = "none";
+    }, 1500); // El modal se cerrará después de 3 segundos
+}
+
+// Cuando el formulario se envía, muestra el modal
+form.onsubmit = function(event) {
+    messageModal.style.display = "none"
+    event.preventDefault(); // Previene el comportamiento predeterminado del formulario
+    mostrarYCerrarModal();
+    // Aquí puedes agregar código adicional para manejar el envío del formulario
+}
+
+// Cierra el modal si el usuario hace clic fuera de él
+window.onclick = function(event) {
+    if (event.target == modalEnvio) {
+        modalEnvio.style.display = "none";
+    }
+}
 
 
 
