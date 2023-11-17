1516fd793fc2b972be007135483a909444b99931
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-11-17 16:14:12 -0500

    Built adminInicio

--- src/views/adminModal.js
+++ src/views/adminModal.js
@@ -1,68 +1,67 @@
-// Obtén el modal
+// Obtén los modales
 var userModal = document.getElementById("userModal");
 var messageModal = document.getElementById("messageModal");
-// Obtén el elemento que abre el modal
-var btn = document.getElementById("openUserModal");
-var openMessageModal = document.getElementById("openMessageModal")
-// Obtén el elemento <span> que cierra el modal
-var span = document.getElementById("closeUserModal");
-var closeMessageModal = document.getElementById("closeMessageModal");
+var modalEnvio = document.getElementById("confirmacionEnvioModal");
 
+// Obtén los elementos que abren los modales
+var openUserModalButtons = document.querySelectorAll("[id^='openUserModal']");
+var openMessageModalButtons = document.querySelectorAll("[id^='openMessageModal']");
+
+
+// Obtén los elementos relacionados con el correo y las notificaciones
 var mail = document.getElementById("mailIcon");
 var mailCount = document.getElementById("mailCount");
-
 var notificationIcon = document.getElementById("notificationIcon");
 var notificationCount = document.getElementById("notificationCount");
 
-var sendMessage = document.getElementById("sendMessage")
-
-// Cuando el usuario hace clic en el botón, abre el modal 
-
-mail.addEventListener("click", function() {
-    window.location.href = "petitions"; 
-});
-
-mailCount.addEventListener("click", function() {
-    window.location.href = "petitions"; 
-});
-
-notificationIcon.addEventListener("click", function() {
-    window.location.href = "history"; 
-});
-
-notificationCount.addEventListener("click", function() {
-    window.location.href = "history"; 
-});
-
-btn.onclick = function () {
-    userModal.style.display = "block";
-}
-
-// Cuando el usuario hace clic en <span> (x), cierra el modal
-span.onclick = function () {
-    userModal.style.display = "none";
-}
-
-// Cuando el usuario hace clic en cualquier lugar fuera del modal, ciérralo
-
-openMessageModal.onclick = function () {
-    messageModal.style.display = "block";
-}
-
-// Cuando el usuario hace clic en <span> (x), cierra el modal
-closeMessageModal.onclick = function () {
-    messageModal.style.display = "none";
-}
-
-// Cuando el usuario hace clic en cualquier lugar fuera del modal, ciérralo
-window.onclick = function (event) {
-    if (event.target == messageModal) {
-        messageModal.style.display = "none";
+// Función para abrir cualquier modal por ID
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "block";
     }
 }
 
-// Obtén el modal
-var modalEnvio = document.getElementById("confirmacionEnvioModal");
+// Función para cerrar cualquier modal por ID
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Asigna manejadores de eventos a los botones que abren los modales
+openUserModalButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        openModal(button.getAttribute("data-modal-id"));
+    });
+});
+
+openMessageModalButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        openModal(button.getAttribute("data-modal-id"));
+    });
+});
+
+
+// Asigna manejadores de eventos al resto del código...
+mail.addEventListener("click", function () {
+    window.location.href = "petitions";
+});
+
+mailCount.addEventListener("click", function () {
+    window.location.href = "petitions";
+});
+
+notificationIcon.addEventListener("click", function () {
+    window.location.href = "history";
+});
+
+notificationCount.addEventListener("click", function () {
+    window.location.href = "history";
+});
+
+// ... Resto del código ...
 
 // Obtén el formulario
 var form = document.getElementById("messageForm");
@@ -70,25 +69,30 @@ var form = document.getElementById("messageForm");
 // Función para mostrar el modal y ocultarlo después de un tiempo
 function mostrarYCerrarModal() {
     modalEnvio.style.display = "block";
-    setTimeout(function() {
+    setTimeout(function () {
         modalEnvio.style.display = "none";
     }, 1500); // El modal se cerrará después de 3 segundos
 }
 
 // Cuando el formulario se envía, muestra el modal
-form.onsubmit = function(event) {
-    messageModal.style.display = "none"
+form.onsubmit = function (event) {
+    messageModal.style.display = "none";
     event.preventDefault(); // Previene el comportamiento predeterminado del formulario
     mostrarYCerrarModal();
+
     // Aquí puedes agregar código adicional para manejar el envío del formulario
-}
+};
 
 // Cierra el modal si el usuario hace clic fuera de él
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modalEnvio) {
-        modalEnvio.style.display = "none";
+        closeModal("confirmacionEnvioModal");
     }
-}
+};
 
-
-
+// Cierra el modal cuando el usuario sale del campo de texto del mensaje
+form.querySelector("input[name='message']").addEventListener("focusout", function () {
+    if (this.value) {
+        modalEnvio.style.display = "block";
+    }
+});
