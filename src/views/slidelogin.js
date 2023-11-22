8d4e72357ba233de13612ef0740958ca0eea0dc7
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-11-22 17:00:22 -0500

    Nuevos cambios en la rama principal

--- src/views/slidelogin.js
+++ src/views/slidelogin.js
@@ -20,10 +20,10 @@ toggle_btn.forEach((btn) => {
   });
 });
 
-function moveSlider() {
-  let index = this.dataset.value;
+let index = 1; // Inicializa el índice en 1
 
-  let currentImage = document.querySelector(`.img-${index}`);
+function moveSlider() {
+  const currentImage = document.querySelector(`.img-${index}`);
   images.forEach((img) => img.classList.remove("show"));
   currentImage.classList.add("show");
 
@@ -31,9 +31,26 @@ function moveSlider() {
   textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
 
   bullets.forEach((bull) => bull.classList.remove("active"));
-  this.classList.add("active");
+  bullets[index - 1].classList.add("active");
+
+  // Incrementa el índice
+  index++;
+
+  // Detén el intervalo si llegamos a la última imagen
+  if (index > images.length) {
+    clearInterval(sliderInterval);
+  }
 }
 
+// Inicia el intervalo para llamar a moveSlider cada 3 segundos
+const sliderInterval = setInterval(moveSlider, 2000);
+
+// Agrega un event listener a cada bullet para mover manualmente el slider
 bullets.forEach((bullet) => {
-  bullet.addEventListener("click", moveSlider);
+  bullet.addEventListener("click", () => {
+    // Reinicia el intervalo al hacer clic en un bullet
+    clearInterval(sliderInterval);
+    index = parseInt(bullet.dataset.value);
+    moveSlider();
+  });
 });
